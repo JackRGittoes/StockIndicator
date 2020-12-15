820abe0f633486cfb22415fe03fa05b1c21c4264
Jack Gittoes <jack.gittoes1@nhs.net>
2020-12-15 13:58:13 +0000

    Add project files.

---
+++ StockIndicator/Currys.cs
@@ -0,0 +1,46 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockIndicator
+{
+    public class Currys
+    {
+        static readonly HttpClient client = new HttpClient();
+        static readonly HtmlDocument htmlDoc = new HtmlDocument();
+        public static async Task<bool> CurrysStockAsync(string url)
+        {
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                var responseBody = await client.GetStringAsync(url);
+                htmlDoc.LoadHtml(responseBody);
+
+                try
+                {
+
+
+                    foreach (var item in htmlDoc.DocumentNode.SelectNodes("//*[@data-button-label]"))
+                    {
+                        if (item.GetAttributeValue("data-button-label", "").Contains("Add to basket"))
+                        {
+                            return true;    
+                        }
+                        return false;
+                    };
+                }
+                catch (NullReferenceException e)
+                {
+                    return false;
+                }
+            }
+            return false;
+
+        }
+    }
+}
---
+++ StockIndicator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using HtmlAgilityPack;
+
+namespace StockIndicator
+{
+    public class Program
+    {
+         
+        public static async Task Main()
+        {
+            
+           Console.WriteLine("Input URL");
+           string url = Console.ReadLine();
+            var result = false;
+            // Infinite loop
+            
+            while (true)
+            {
+                if (url.ToLower().Contains("currys"))
+                {
+                   result = await Currys.CurrysStockAsync(url);
+                }
+                
+                if (result == false)
+                {
+                    Console.WriteLine("\n Out of stock");
+                }
+                else
+                {
+                    Console.WriteLine("\n In stock");
+                }
+                Thread.Sleep(5000);
+            }
+
+        }
+
+        
+    }
+}
